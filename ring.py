b77e884cea7e2669e54da7a037d072a873c39876
RDornodon <archelf.hun@gmail.com>
2021-10-01 15:11:33 +0200

    after-submit flex 2

--- ring.py
+++ ring.py
@@ -1,1 +1,1 @@
-for I in[I:=input]*int(I()):L=int(I());A=abs;G=L//2;X={'L':(0,L-1),'R':(G-1,G),'U':(0,G-1),'D':(G,L-1)}[I()];D=I()+I()[::-1];U=D.index('2');Q=[(B:=-A(A(i-U)-G)+G,B-A(A(i-x)-G)+G)[::-1]for i in range(L)for x in X if'1'==D[i]];print(min(Q)[1])
+for I in[I:=input]*int(I()):A,L,F,D=abs,int(I()),I(),I()+I()[::-1];G=L//2;R=0,G-1,G,L-1,0;print(min([(B:=G-A(A(i-D.index('2'))-G),B-A(A(i-x)-G)+G)[::-1]for i in range(L)for x in dict(zip('URDL',zip(R,R[1:])))[F]if'1'==D[i]])[1])
