30d94c2bacc3c000bb9c8f3ebceb74e3bf2409d7
RDornodon <archelf.hun@gmail.com>
2021-10-01 11:24:27 +0200

    after-submit flex

--- ring.py
+++ ring.py
@@ -1,1 +1,1 @@
-for I in[I:=input]*int(I()):L=int(I());A=abs;G=L//2;X={'L':(0,L-1),'R':(G-1,G),'U':(0,G-1),'D':(G,L-1)}[I()];D=I()+I()[::-1];U=D.index('2');Q=[(B:=-A(A(i-U)-G)+G,B-A(A(i-x)-G)+G)for i in range(L)for x in X if'1'==D[i]];print(min(b for b,c in Q if c==min(a[1]for a in Q)))
+for I in[I:=input]*int(I()):L=int(I());A=abs;G=L//2;X={'L':(0,L-1),'R':(G-1,G),'U':(0,G-1),'D':(G,L-1)}[I()];D=I()+I()[::-1];U=D.index('2');Q=[(B:=-A(A(i-U)-G)+G,B-A(A(i-x)-G)+G)[::-1]for i in range(L)for x in X if'1'==D[i]];print(min(Q)[1])
